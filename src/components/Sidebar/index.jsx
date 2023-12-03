7b187e7d9bd01f60b0295364ec307fc36eb18b56
Awon Abbas <awonabbas2@gmail.com>
2023-12-03 11:45:29 +0500

    content of the side bar change according to role

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,106 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ role }) => {
+  const getSidebarContent = () => {
+    switch (role) {
+      case "teacher":
+        return (
+          <>
+            <li>
+              <NavLink to="/dashboard" activeClassName="active" exact>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/attendance-management">
+                Attendance Management
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/record-attendance">Record Attendance</NavLink>
+            </li>
+            <li>
+              <NavLink to="/grades">Grades Management</NavLink>
+            </li>
+            <li>
+              <NavLink to="/make-assignment">Make Assignment</NavLink>
+            </li>
+            <li>
+              <NavLink to="/time-table">Time Table</NavLink>
+            </li>
+            <li>
+              <NavLink to="/feedback">Feedback</NavLink>
+            </li>
+          </>
+        );
+      case "student":
+        return (
+          <>
+            <li>
+              <NavLink to="/dashboard" activeClassName="active" exact>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/attendance-management">
+                Attendance Management
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/record-attendance">Record Attendance</NavLink>
+            </li>
+            <li>
+              <NavLink to="/grades">Grades Management</NavLink>
+            </li>
+            <li>
+              <NavLink to="/make-assignment">Make Assignment</NavLink>
+            </li>
+            <li>
+              <NavLink to="/time-table">Time Table</NavLink>
+            </li>
+            <li>
+              <NavLink to="/feedback">Feedback</NavLink>
+            </li>
+          </>
+        );
+
+      case "admin":
+        return (
+          <>
+            <li>
+              <NavLink to="/dashboard" activeClassName="active" exact>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/attendance-management">
+                Attendance Management
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/record-attendance">Record Attendance</NavLink>
+            </li>
+            <li>
+              <NavLink to="/grades">Grades Management</NavLink>
+            </li>
+            <li>
+              <NavLink to="/make-assignment">Make Assignment</NavLink>
+            </li>
+            <li>
+              <NavLink to="/time-table">Time Table</NavLink>
+            </li>
+            <li>
+              <NavLink to="/feedback">Feedback</NavLink>
+            </li>
+          </>
+        );
+
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="deznav">
       <div className="deznav-scroll">
@@ -15,33 +114,7 @@ const Sidebar = () => {
               <i className="flaticon-dashboard-1"></i>
               <span className="nav-text">Dashboard</span>
             </NavLink>
-            <ul aria-expanded="false">
-              <li>
-                <NavLink to="/dashboard" activeClassName="active" exact>
-                  Dashboard
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/attendance-management">
-                  Attendance Management
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/record-attendance">Record Attendance</NavLink>
-              </li>
-              <li>
-                <NavLink to="/grades"> Grades Management</NavLink>
-              </li>
-              <li>
-                <NavLink to="/make-assignment">Make Assignment</NavLink>
-              </li>
-              <li>
-                <NavLink to="/time-table">Time Table</NavLink>
-              </li>
-              <li>
-                <NavLink to="/feedback">Feedback</NavLink>
-              </li>
-            </ul>
+            <ul aria-expanded="false">{getSidebarContent()}</ul>
           </li>
         </ul>
       </div>
